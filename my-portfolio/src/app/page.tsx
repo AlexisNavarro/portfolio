b14fc00d12549812b5cfc7fa14810cb8402f5f73
Alexis Navarro <navarroalexis007@gmail.com>
2025-04-14 05:17:44 -0600

    added about me at the home page

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -22,12 +22,22 @@ export default function Home() {
 
       {/*Text containing name and roles*/}
       {/*Main heading*/}
-      <h1 className="text-4xl font-bold"> My Name is Alexis Navarro</h1>
+      <h1 className="text-4xl font-bold">Alexis Navarro</h1>
 
       {/*Sub heading*/}
-      <p className="mt-4 text-xl text-gray-600">
+      <p className="mt-4 text-xl text-white-600">
         Software Engineer | Cybersecurity | Full-Stack developer | Frontend Developer | Backend Developer | Database Management
       </p>
+
+      {/* about me section */}
+      <section id = "about" className="max-w-2xl mt-12 px-4 text left">
+        <h2 className="text-2xl font-bold mb-4 text-blue-700">About Me</h2>
+        <p className = "text-white-700 text-lg leading-relaxed">
+          Hey! I'm Alexis Navarro, a software Engineer with a  Master's degree in Software Engineering and a strong foundation in cybersecurity,
+          backend/frontend development, and full-stack solutions. I've built scalable applications for Pacific Northwest National laboratory, DEVCOM, and tech startups such as Hiller Measurments,
+          and I thrive in collaborative, agile environments.
+        </p>
+      </section>
     </main>
   );
 }//end of home
