16629cc7bfa383a8bae53c370cc7fb36d42a8c3c
Alexis Navarro <navarroalexis007@gmail.com>
2025-04-17 17:46:29 -0600

    fixed typos and added info to the PNNL section

--- my-portfolio/src/app/experience/page.tsx
+++ my-portfolio/src/app/experience/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function ExperiencePage() {
   return (
     <main className="min-h-screen p-8 pt-20 text-center bg-cover bg-center"
-    style={{backgroundImage: 'url("/backgrounds/background.webp")'}}
+      style={{ backgroundImage: 'url("/backgrounds/background.webp")' }}
     >
       <h1 className="text-4xl font-bold mb-8">Professional Experience</h1>
 
@@ -31,12 +31,13 @@ export default function ExperiencePage() {
           <li>Reduced onboarding time by 80% with clear technical docs in Figma & MS Office</li>
           <li>Learned proper coding standards and utilization of Git for version control within a large team</li>
         </ul>
+
         <h2 className="text-lg font-medium mt-4 mb-4">Technologies Used</h2>
         <div className="flex flex-wrap gap-3">
           {[
             "DBeaver", "Docker", "Git", "Gitlab", "Node.js",
             "Python", "React", "RESTful APIs", "SQL",
-            "SQLAlchemy", "TypeScript", "JIRA", "Agile"
+            "SQLAlchemy", "Pandas", "TypeScript", "JIRA", "Agile", "Figma", "MS Office"
           ].map((tech) => (
             <span key={tech} className="bg-blue-600 px-3 py-1 rounded-full text-sm">
               {tech}
@@ -68,10 +69,27 @@ export default function ExperiencePage() {
           Full Stack Software Engineer | August 2023 - December 2023
         </p>
         <ul className="list-disc list-inside mt-2 text-white-700">
-          <li>Built front-end features using TypeScript, HTML, and CSS</li>
-          <li>Collaborated with researchers to visualize climate data</li>
-          <li>Delivered fully tested components in an agile team</li>
+          <li>Collaborated with Master's students to develop a new version of Home Assistant in an Agile environment</li>
+          <li>Built front-end features using React, TypeScript, HTML, and CSS</li>
+          <li>Developed 50 backend functions with Python and Node.js</li>
+          <li>Designed 30+ SQL schemas to meet evolving team and application requirements</li>
+          <li>Migrated legacy records to AWS RDS, enhancing database security by 80%</li>
+          <li>Configured AWS IAM for role-based access controls</li>
+          <li>Delivered fully tested features as part of an Agile development team</li>
         </ul>
+
+        <h2 className="text-lg font-medium mt-4 mb-4">Technologies Used</h2>
+        <div className="flex flex-wrap gap-3">
+          {[
+            "AWS", "AWS-RDS", "AWS-IAM", "Git",
+            "Node.js", "Python", "React", "RESTful APIs",
+            "SQL", "TypeScript", "HTML", "CSS", "Agile"
+          ].map((tech) => (
+            <span key={tech} className="bg-blue-600 px-3 py-1 rounded-full text-sm">
+              {tech}
+            </span>
+          ))}
+        </div>
       </div>
 
 
@@ -82,7 +100,7 @@ export default function ExperiencePage() {
           {/*Devcom logo */}
 
           <Image src="/logos/devcom_logo.webp"
-            alt="Hiller Measurments Logo"
+            alt="Devcom Logo"
             width={80}
             height={80}
             className="object-contain rounded-md" />
