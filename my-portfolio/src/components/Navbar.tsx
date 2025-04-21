dda2d1db164660828db3093fba6e59caea34d6cc
Alexis Navarro <navarroalexis007@gmail.com>
2025-04-20 19:51:51 -0600

    added routing for education page

--- my-portfolio/src/components/Navbar.tsx
+++ my-portfolio/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav className="bg-blue-600 text-white px-6 py-4 shadow-md fixed top-0 left-0 w-full z-10">
       <div className="max-w-7xl mx-auto flex justify-end space-x-8">
         <Link href="/" className="hover:underline">Main Page</Link>
-        <Link href="#education" className="hover:underline">Education</Link>
+        <Link href="/education" className="hover:underline">Education</Link>
         <Link href="/experience" className="hover:underline">Experience</Link>
         <Link href="#contact" className="hover:underline">Contact</Link>
       </div>
