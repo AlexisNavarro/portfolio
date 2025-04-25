d04fadd51c5abd0715898504d1edd2611315a508
Alexis Navarro <navarroalexis007@gmail.com>
2025-04-25 16:57:56 -0600

    created contact folder and page

---
+++ my-portfolio/src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "next/image";
+
+export default function ContactPage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-8 text-center pt-20 bg-cover bg-center"
+        style={{backgroundImage: 'url("/backgrounds/contact_background.jpg'}}
+        >
+
+        </main>
+
+    );
+}
