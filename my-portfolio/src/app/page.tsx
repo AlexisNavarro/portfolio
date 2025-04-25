5f5efac6a2ec7bcc3afbc79a55efac19874afd5f
Alexis Navarro <navarroalexis007@gmail.com>
2025-04-25 17:03:49 -0600

    removed navbar tag, since navbar is in the layout

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -5,10 +5,9 @@ export default function Home() {
   const profilePic = "/Alexis_Navarro_Pic.jpg"
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8 text-center pt-20 bg-cover bg-center"
-    style={{backgroundImage: 'url("/backgrounds/home_background3.jpg'}}
+      style={{ backgroundImage: 'url("/backgrounds/home_background3.jpg' }}
     >
 
-      <Navbar />
 
       {/*Profile Picture aka Alexis */}
       <div className="flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-8">
@@ -33,22 +32,22 @@ export default function Home() {
       </p>
 
       {/* about me section */}
-      <section id = "about" className="max-w-2xl mt-12 px-4 text-left">
+      <section id="about" className="max-w-2xl mt-12 px-4 text-left">
         <h2 className="text-2xl font-bold mb-4 text-white-700">About Me</h2>
-        <p className = "text-white-700 text-lg leading-relaxed">
+        <p className="text-white-700 text-lg leading-relaxed">
           Hey! I'm Alexis Navarro, a software Engineer with a  Master's degree in Software Engineering and a strong foundation in cybersecurity,
           backend/frontend development, and full-stack solutions. I've built scalable applications for Pacific Northwest National laboratory, DEVCOM, and tech startups such as Hiller Measurements,
           and I thrive in collaborative, agile environments.
         </p>
 
         <section className="mt-8 max max-w-2xl text-left">
-          <h2 className= "text-2xl font-semibold mb-4">Notable Tech Stack</h2>
+          <h2 className="text-2xl font-semibold mb-4">Notable Tech Stack</h2>
           <div className="flex flex-wrap gap-3">
-          {["Python", "Java","Typescript","React", "Node.js", "SQL", "AWS", "Docker", "MongoDB", "TypeScript", "PostgreSQL", "Git", "Jenkins", "Flask"].map((tech)=>(
-            <span key = {tech} className="bg-blue-600 px-3 py-1 rounded-full text-sm">
-              {tech}
-            </span>
-          ))}
+            {["Python", "Java", "Typescript", "React", "Node.js", "SQL", "AWS", "Docker", "MongoDB", "TypeScript", "PostgreSQL", "Git", "Jenkins", "Flask"].map((tech) => (
+              <span key={tech} className="bg-blue-600 px-3 py-1 rounded-full text-sm">
+                {tech}
+              </span>
+            ))}
           </div>
         </section>
       </section>
