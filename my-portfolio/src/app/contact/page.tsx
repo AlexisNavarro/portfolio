791e967d5b3b65bac8a5f62955fd6a32e9ebbf07
Alexis Navarro <navarroalexis007@gmail.com>
2025-04-26 21:46:40 -0600

    added download link for resume and changed text color to black for links

--- my-portfolio/src/app/contact/page.tsx
+++ my-portfolio/src/app/contact/page.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import Image from "next/image";
+import { Download } from "lucide-react";
+
 
 export default function ContactPage() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-8 text-center pt-20 bg-cover bg-center"
-        style={{backgroundImage: 'url("/backgrounds/contact_background.jpg'}}
+            style={{ backgroundImage: 'url("/backgrounds/contact_background.jpg' }}
         >
             <h1 className="text-4xl font-bold mb-12 drop-shadow-lg text-white">Contact Me</h1>
 
-            <div className="max-w-2xlmx-auto space-y-10 bg-white/20 backdrop-blur-md p-10 rounded-2xl shadow-xl text-white">
+            <div className="max-w-2xl mx-auto space-y-10 bg-white/20 backdrop-blur-md p-10 rounded-2xl shadow-xl text-white">
 
                 {/**email */}
 
                 <div>
                     <h2 className="text-2xl font-semibold mb-2">Email</h2>
-                    <a href="mailto:navarroalexis007@gmail.com" className="text-black-800 hover:underline">
+                    <a href="mailto:navarroalexis007@gmail.com" className="text-black hover:underline">
                         navarroalexis007@gmail.com
                     </a>
                 </div>
@@ -22,8 +24,8 @@ export default function ContactPage() {
                 {/**linkedin */}
                 <div>
                     <h2 className="text-2xl font-semibold mb-2">Linkedin</h2>
-                    <a href="https://www.linkedin.com/in/AlexisNavarro99" target= "_blank" className="text-blue-700 hover:underline">
-                    linkedin.com/in/AlexisNavarro99
+                    <a href="https://www.linkedin.com/in/AlexisNavarro99" target="_blank" className="text-black hover:underline">
+                        linkedin.com/in/AlexisNavarro99
                     </a>
                 </div>
 
@@ -31,14 +33,25 @@ export default function ContactPage() {
 
                 <div>
                     <h2 className="text-2xl font-semibold mb-2">Github</h2>
-                    <a href="https://github.com/AlexisNavarro"  target= "_blank" className="text-blue-300 hover:underline">
+                    <a href="https://github.com/AlexisNavarro" target="_blank" className="text-black hover:underline">
                         navarroalexis007@gmail.com
+                    </a>
+                </div>
+
+                <div className=" flex items-center space-x-4 justify-center">
+                    <Download className="text-blue-300" size={30} />
+                    <a
+                        href="/documents/Alexis_Navarro_2025_Resume.pdf"
+                        download
+                        className="inline-block bg-blue-500 hover:bg-blue-600 text-black font-bold py-2 px-6 rounded-lg transition duration-300"
+                    >
+                        Download Resume
                     </a>
                 </div>
 
 
 
-                
+
             </div>
 
         </main>
