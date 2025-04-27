514936f199ca98802eb6caa70b36e31c48bb485f
Alexis Navarro <navarroalexis007@gmail.com>
2025-04-26 21:32:40 -0600

    added email, linkedin, and github links

--- my-portfolio/src/app/contact/page.tsx
+++ my-portfolio/src/app/contact/page.tsx
@@ -6,6 +6,40 @@ export default function ContactPage() {
         <main className="flex min-h-screen flex-col items-center justify-center p-8 text-center pt-20 bg-cover bg-center"
         style={{backgroundImage: 'url("/backgrounds/contact_background.jpg'}}
         >
+            <h1 className="text-4xl font-bold mb-12 drop-shadow-lg text-white">Contact Me</h1>
+
+            <div className="max-w-2xlmx-auto space-y-10 bg-white/20 backdrop-blur-md p-10 rounded-2xl shadow-xl text-white">
+
+                {/**email */}
+
+                <div>
+                    <h2 className="text-2xl font-semibold mb-2">Email</h2>
+                    <a href="mailto:navarroalexis007@gmail.com" className="text-black-800 hover:underline">
+                        navarroalexis007@gmail.com
+                    </a>
+                </div>
+
+                {/**linkedin */}
+                <div>
+                    <h2 className="text-2xl font-semibold mb-2">Linkedin</h2>
+                    <a href="https://www.linkedin.com/in/AlexisNavarro99" target= "_blank" className="text-blue-700 hover:underline">
+                    linkedin.com/in/AlexisNavarro99
+                    </a>
+                </div>
+
+                {/**Github */}
+
+                <div>
+                    <h2 className="text-2xl font-semibold mb-2">Github</h2>
+                    <a href="https://github.com/AlexisNavarro"  target= "_blank" className="text-blue-300 hover:underline">
+                        navarroalexis007@gmail.com
+                    </a>
+                </div>
+
+
+
+                
+            </div>
 
         </main>
 
