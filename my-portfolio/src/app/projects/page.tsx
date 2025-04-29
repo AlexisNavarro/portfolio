d25a9f0950d7eac332588834c584576203d48fdc
Alexis Navarro <navarroalexis007@gmail.com>
2025-04-29 15:19:50 -0600

    created projects folder and file, included a background image to start

---
+++ my-portfolio/src/app/projects/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Image from "next/image";
+
+export default function ProjectPage(){
+    return(
+        <main className="flex min-h-screen flex-col items-center justify-center p-8 text-center pt-20 bg-cover bg-center"
+        style={{backgroundImage: 'url("/backgrounds/projects_background.jpg'}}>
+
+        </main>
+    );
+}
